cb8b73190a004ae32270b97aab21a769bf1c0995
Anartz Mugika <mugan86@gmail.com>
2017-08-01 20:23:25 +0200

    Load select flag drawable in select money item

--- app/src/main/java/amldev/currency/ui/adapters/MoneyAdapter.kt
+++ app/src/main/java/amldev/currency/ui/adapters/MoneyAdapter.kt
@@ -2,28 +2,31 @@ package amldev.currency.ui.adapters
 
 import amldev.currency.R
 import amldev.currency.ui.utils.ctx
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import domain.model.Money
 import kotlinx.android.synthetic.main.item_money.view.*
 
 
-/**
+/***************************************************************
  * Created by anartzmugika on 31/7/17.
- */
+ *
+ * Adapter to load money info in RecyclerView List with custom flag
+ *******************************************************************/
 class MoneyAdapter (
         val moneyList: ArrayList<Money>, val itemClick: (Money) -> Unit)
     : RecyclerView.Adapter<MoneyAdapter.ViewHolder>() {
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_money, parent, false)
         return ViewHolder(view, itemClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        //Bind custom item (define in item_forecast.xml)
+        //Bind custom item (define in item_money.xml)
         holder.bindForecast(moneyList[position])
     }
 
@@ -34,12 +37,14 @@ class MoneyAdapter (
 
         fun bindForecast(money: Money) {
             with(money) {
-                // Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                Picasso.with(itemView.ctx).load(getFlagDrawable(itemView.ctx, flag)).into(itemView.icon)
                 itemView.symbolTextView.text = symbol
                 itemView.nameTextView.text = name
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
+        private fun getFlagDrawable(context: Context, flag: String) =
+                context.resources.getIdentifier("ic_$flag", "drawable", context.packageName)
     }
 
     interface OnItemClickListener {
