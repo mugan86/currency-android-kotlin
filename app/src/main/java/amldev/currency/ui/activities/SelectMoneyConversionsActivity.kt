32890b4eda6f8c1b9ab38ca129762c727fc42027
Anartz Mugika <mugan86@gmail.com>
2017-08-03 19:27:19 +0200

    Start to create select money conversions with all coins. Start to define layout with top info (selec money), ads banner and other moneys gridview

---
+++ app/src/main/java/amldev/currency/ui/activities/SelectMoneyConversionsActivity.kt
@@ -0,0 +1,14 @@
+package amldev.currency.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import amldev.currency.R
+
+class SelectMoneyConversionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_money_conversions)
+    }
+}
