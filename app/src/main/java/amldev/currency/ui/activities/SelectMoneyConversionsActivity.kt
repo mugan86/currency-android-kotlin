327420b3c3ffd4826e3a4df57432bce72c1741d1
Anartz Mugika <mugan86@gmail.com>
2017-08-05 08:29:10 +0200

    Apply changes to take conversion data in real time

--- app/src/main/java/amldev/currency/ui/activities/SelectMoneyConversionsActivity.kt
+++ app/src/main/java/amldev/currency/ui/activities/SelectMoneyConversionsActivity.kt
@@ -4,9 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 import amldev.currency.R
-import android.widget.AdapterView
-import android.widget.Toast
 import com.kotlin.amugika.gridview.ui.adapter.MoneysConversionsCustomGrid
+import data.CurrencyRequest
 import domain.commands.RequestCurrencyCommand
 import kotlinx.android.synthetic.main.activity_select_money_conversions.*
 import org.jetbrains.anko.doAsync
@@ -14,7 +13,6 @@ import org.jetbrains.anko.indeterminateProgressDialog
 import org.jetbrains.anko.uiThread
 
 class SelectMoneyConversionsActivity : AppCompatActivity() {
-    internal var symbols = arrayOf("AUD", "CAD", "CHF", "CNY", "EUR", "GBP", "INR", "JPY", "MYR", "RUB", "SGD", "USD")
     internal var imageId = intArrayOf(R.drawable.ic_australia, R.drawable.ic_canada, R.drawable.ic_china, R.drawable.ic_switzerland, R.drawable.ic_europe, R.drawable.ic_united_kingdom, R.drawable.ic_india, R.drawable.ic_japan, R.drawable.ic_malaysia, R.drawable.ic_russia, R.drawable.ic_singapore, R.drawable.ic_uuss)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,19 +39,22 @@ class SelectMoneyConversionsActivity : AppCompatActivity() {
 
         inputConvertInfoTextView.text = "Input value to convert with $symbol: "
 
-        //TODO Take select symbol and quit money to show in
         val progress = indeterminateProgressDialog("This a progress dialog")
 
         doAsync {
             progress.show()
             var result = RequestCurrencyCommand(symbol).execute();
-            println(result.size())
+            println(result)
             println(result.getSelectMoneyCurrency("USD").currencyValue)
 
+            println()
+
+
             uiThread {
-                val adapter = MoneysConversionsCustomGrid(symbols, imageId)
+                val moneys = CurrencyRequest().getMoneyList(this@SelectMoneyConversionsActivity).filter{ it.symbol != symbol}
+                val adapter = MoneysConversionsCustomGrid(moneys, result, 1.0) //TODO use edittext value
                 conversionOtherMoneyGridView.adapter = adapter
-                conversionOtherMoneyGridView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id -> Toast.makeText(parent.context, "You Clicked at " + symbols[+position], Toast.LENGTH_SHORT).show() }
+                // conversionOtherMoneyGridView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id -> Toast.makeText(parent.context, "You Clicked at " + moneys[+position].name, Toast.LENGTH_SHORT).show() }
                 progress.dismiss()
             }
         }
