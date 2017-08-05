05c9e85473b83fbc8d70777d51d150daa74fa498
Anartz Mugika <mugan86@gmail.com>
2017-08-05 08:27:49 +0200

    Add icon property in Money

--- app/src/main/java/amldev/currency/domain/model/DomainClasses.kt
+++ app/src/main/java/amldev/currency/domain/model/DomainClasses.kt
@@ -1,7 +1,9 @@
 package domain.model
 
+import amldev.currency.R
+
 /***************************************************************************************************************
- * Created by Anartz Mugika on 19/07/2017.
+ * Created by Anartz Mugika on 19/07/2017. Updated in 05/08/2017.
  * Finish result model definition to use in Currency result
  **************************************************************************************************************/
 data class Currency (val baseMoneySymbol: String, val baseMoneyName: String,
@@ -17,5 +19,9 @@ data class Currency (val baseMoneySymbol: String, val baseMoneyName: String,
 
 }
 
-data class Money (val symbol: String = "", val currencyValue: Double = 0.0, val name: String = "", val flag: String = "united_nations")
+data class Money (val symbol: String = "",
+                  val currencyValue: Double = 0.0,
+                  val name: String = "",
+                  val flag: String = "united_nations",
+                  val icon: Int = R.drawable.ic_united_nations)
 
