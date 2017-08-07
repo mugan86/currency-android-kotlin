17a792f2a9488d022df25e172a53f5f31a1155b3
Anartz Mugika <mugan86@gmail.com>
2017-08-07 19:33:46 +0200

    Add default share intent option

--- app/src/main/java/amldev/currency/utils/BasicOptionsExtensions.kt
+++ app/src/main/java/amldev/currency/utils/BasicOptionsExtensions.kt
@@ -1,7 +1,14 @@
 package amldev.currency.utils
 
+import amldev.currency.R
 import android.content.Context
 import android.net.ConnectivityManager
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 /**************************************************************************************************
@@ -27,4 +34,46 @@ fun isNetworkConnected(context: Context): Boolean {
      * Checking if the device is connected to an available network connection.
      **************************************************************************************************/
     return networkInfo != null && networkInfo.isConnected // 3
+}
+
+fun getDefaultShareIntent(context: Context, bm: Bitmap?): Intent {
+
+    val intent = Intent(Intent.ACTION_SEND)
+    if (bm == null) {
+        intent.type = "text/plain"
+    } else {
+        intent.type = "image/*"
+    }
+    //
+
+    val subject: String
+    val text: String
+
+    subject = context.resources.getString(R.string.share_chooser_title)
+
+    text = context.resources.getString(R.string.share_message)
+
+
+    if (bm != null) {
+        // Save this bitmap to a file.
+        val cache = context.externalCacheDir
+        val sharefile = File(cache, "toshare.png")
+        try {
+            val out = FileOutputStream(sharefile)
+            assert(bm != null)
+            bm.compress(Bitmap.CompressFormat.PNG, 100, out)
+            out.flush()
+            out.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + sharefile))
+    }
+
+    intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+    intent.putExtra(Intent.EXTRA_TEXT, text)
+
+    //Start social networks window
+    return Intent.createChooser(intent, context.resources.getString(R.string.share_chooser_title))
 }
