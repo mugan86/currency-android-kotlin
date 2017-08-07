2f482b4c7ba4e2259da5833f798fc6baa6f5f320
Anartz Mugika <mugan86@gmail.com>
2017-08-07 08:24:16 +0200

    Check network state

---
+++ app/src/main/java/amldev/currency/utils/BasicOptionsExtensions.kt
@@ -0,0 +1,29 @@
+package amldev.currency.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+
+/**************************************************************************************************
+ * Created by Anartz Mugika on 7/8/17.
+ *************************************************************************************************/
+fun isNetworkConnected(context: Context): Boolean {
+    /**************************************************************************************************
+    * Retrieving an instance of the ConnectivityManager class from the current application context.
+    * The ConnectivityManager class is simply your go to class if you need any information about the
+    * state of network connectivity. It can also be set up to report network connection
+    * changes to your application.
+    **************************************************************************************************/
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    /**************************************************************************************************
+     * Retrieving information about the current active network connection.
+     * This will be null if there's no network connection.
+    **************************************************************************************************/
+    val networkInfo = connectivityManager.activeNetworkInfo // 2
+
+    /**************************************************************************************************
+     * Checking if the device is connected to an available network connection.
+     **************************************************************************************************/
+    return networkInfo != null && networkInfo.isConnected // 3
+}
