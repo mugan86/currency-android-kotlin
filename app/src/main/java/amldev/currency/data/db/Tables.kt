6afbcda611cd8c83cb7cd367eecbfc3480d88a75
Anartz Mugika <mugan86@gmail.com>
2017-08-21 11:35:38 +0200

    Create Currencies App Tables

---
+++ app/src/main/java/amldev/currency/data/db/Tables.kt
@@ -0,0 +1,19 @@
+package amldev.currency.data.db
+
+/**
+ * Created by anartzmugika on 21/8/17.
+ */
+
+object MoneyInfoTable {
+    val NAME = "name"
+    val ID = "_id"
+    val SYMBOL = "symbol"
+    val FLAG = "flag"
+}
+
+object MoneyCurrenciesTable {
+    val UPDATED_DATE = "updated_date"
+    val ID_BASE = "_id_base"
+    val ID_CONVERSION_MONEY = "_id_conversion_money"
+    val VALUE_CONVERSION = "value_conversion"
+}
