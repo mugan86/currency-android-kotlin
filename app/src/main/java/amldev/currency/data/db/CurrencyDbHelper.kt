d1402e04dd24900145b8e993271b3a4fcaff5bfe
Anartz Mugika <mugan86@gmail.com>
2017-08-21 11:57:17 +0200

    Create CurrencyDBHelper to manage SQLite Database

---
+++ app/src/main/java/amldev/currency/data/db/CurrencyDbHelper.kt
@@ -0,0 +1,38 @@
+package amldev.currency.data.db
+
+import amldev.currency.ui.App
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+/**
+ * Created by anartzmugika on 21/8/17.
+ */
+class CurrencyDbHelper : ManagedSQLiteOpenHelper(App.instance,
+        CurrencyDbHelper.DB_NAME, null, CurrencyDbHelper.DB_VERSION) {
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(MoneyInfoTable.NAME, true)
+        db.dropTable(MoneyCurrenciesTable.NAME, true)
+        onCreate(db)
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(MoneyInfoTable.NAME, true,
+                MoneyInfoTable.ID to TEXT + PRIMARY_KEY,
+                MoneyInfoTable.MONEY to TEXT,
+                MoneyInfoTable.FLAG to TEXT,
+                MoneyInfoTable.SYMBOL to TEXT)
+
+        db.createTable(MoneyCurrenciesTable.NAME, true,
+                MoneyCurrenciesTable.ID_BASE to TEXT + PRIMARY_KEY,
+                MoneyCurrenciesTable.ID_CONVERSION_MONEY to TEXT + PRIMARY_KEY,
+                MoneyCurrenciesTable.VALUE_CONVERSION to TEXT,
+                MoneyCurrenciesTable.UPDATED_DATE to TEXT)    }
+
+    companion object {
+        val DB_NAME = "currency.db"
+        val DB_VERSION = 1
+        val instance by lazy { CurrencyDbHelper() }
+    }
+
+}
