55a7400a991037b150736e282c61436bd2596759
Anartz Mugika <mugan86@gmail.com>
2017-08-22 10:03:30 +0200

    Define DB Classes

--- app/src/main/java/amldev/currency/data/db/DbClasses.kt
+++ app/src/main/java/amldev/currency/data/db/DbClasses.kt
@@ -5,10 +5,29 @@ package amldev.currency.data.db
  */
 
 
-class CurrencyDb(
-        val dbHelper: CurrencyDbHelper = CurrencyDbHelper.instance, val dataMapper: DbDataMapper = DbDataMapper()) {
+class MoneyInfo(var map: MutableMap<String, Any?>) {
+    // Properties must be equal tables columns names
+    var money: String by map
+    var symbol: String by map
+    var flag: String by map
 
-    fun test() {
-        dbHelper.use {  }
+    constructor(money: String, symbol: String, flag: String) : this(HashMap()) {
+        this.money = money
+        this.symbol = symbol
+        this.flag = flag
+    }
+}
+
+class MoneyCurrencies (var map: MutableMap<String, Any?>) {
+    var _id_base: String by map
+    var _id_conversion_money: String by map
+    var updated_date: String by map
+    var value_conversion: String by map
+
+    constructor(_id_base: String, _id_conversion_money: String, updated_date: String, value_conversion: String): this(HashMap()) {
+        this._id_base = _id_base
+        this._id_conversion_money = _id_conversion_money
+        this.updated_date = updated_date
+        this.value_conversion = value_conversion
     }
 }
