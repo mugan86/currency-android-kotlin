2b24503bc24f2386b45c00a0b30bdaa12326d06b
Anartz Mugika <mugan86@gmail.com>
2017-08-22 07:39:50 +0200

    Move ActionExtensions to extensions package from /ui/utils

--- app/src/main/java/amldev/currency/extensions/ActionExtensions.kt
+++ app/src/main/java/amldev/currency/extensions/ActionExtensions.kt
@@ -1,8 +1,12 @@
-package amldev.currency.ui.utils
+package amldev.currency.extensions
 
 import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import com.google.gson.JsonParser
+import java.io.InputStreamReader
 
 /**
  * Created by anartzmugika on 6/8/17.
@@ -15,4 +19,18 @@ fun showHideKeyBoardForce(editText: EditText, show: Boolean, context: Context) {
         inputMethodManager.toggleSoftInputFromWindow(editText.getApplicationWindowToken(), InputMethodManager.SHOW_FORCED, 0)
     }
     else inputMethodManager.toggleSoftInputFromWindow(editText.getApplicationWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+}
+
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+fun getJSONResource(context: Context, name: String): String? {
+    try {
+        context.getAssets().open("$name.json").use({ `is` ->
+            val parser = JsonParser()
+            return parser.parse(InputStreamReader(`is`)).toString()
+        })
+    } catch (e: Exception) {
+
+    }
+
+    return null
 }
