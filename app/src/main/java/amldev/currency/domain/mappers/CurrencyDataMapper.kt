c18b1a506b58379d50af17dffc6171ee57e28c9e
Anartz Mugika <mugan86@gmail.com>
2017-08-22 08:11:33 +0200

    Clean code and optimize

--- app/src/main/java/amldev/currency/domain/mappers/CurrencyDataMapper.kt
+++ app/src/main/java/amldev/currency/domain/mappers/CurrencyDataMapper.kt
@@ -14,28 +14,29 @@ import java.util.*
  * Call from "RequestCurrencyCommand". In this class map correctly to our preferences to show in final result
  ***********************************************************************************************************************/
 class CurrencyDataMapper {
-    fun convertFromDataModel(currency: CurrencyResult) : Currency{
+
+    fun convertFromDataModel(currency: CurrencyResult) = with(currency) {
         // Pass money name
-        return Currency(currency.base, ""
-                , getMoneyConversions(currency.rates), convertDate(Calendar.getInstance().timeInMillis))
+        Currency(base, ""
+                , getMoneyConversions(rates), convertDate(Calendar.getInstance().timeInMillis))
     }
 
     private fun convertDate(date: Long) = DateFormat.getDateInstance(DateFormat.FULL, Locale.getDefault()).format(date)
 
-    private fun getMoneyConversions(rates: Rates): List<Money> {
-        val symbols = CurrencyRequest.symbols.split(",".toRegex())
-        return listOf<Money>(Money(symbols[0], rates.AUD),
-                                Money(symbols[1], rates.CAD),
-                                Money(symbols[2], rates.CHF),
-                                Money(symbols[3], rates.CNY),
-                                Money(symbols[4], rates.EUR),
-                                Money(symbols[5], rates.GBP),
-                                Money(symbols[6], rates.INR),
-                                Money(symbols[7], rates.JPY),
-                                Money(symbols[8], rates.MYR),
-                                Money(symbols[9], rates.RUB),
-                                Money(symbols[10], rates.SGD),
-                                Money(symbols[11], rates.USD)
-                )
+    private fun getMoneyConversions(rates: Rates) = with(rates) {
+            val symbols = CurrencyRequest.symbols.split(",".toRegex())
+            listOf<Money>(Money(symbols[0], AUD),
+                    Money(symbols[1], CAD),
+                    Money(symbols[2], CHF),
+                    Money(symbols[3], CNY),
+                    Money(symbols[4], EUR),
+                    Money(symbols[5], GBP),
+                    Money(symbols[6], INR),
+                    Money(symbols[7], JPY),
+                    Money(symbols[8], MYR),
+                    Money(symbols[9], RUB),
+                    Money(symbols[10], SGD),
+                    Money(symbols[11], USD)
+            )
     }
 }
