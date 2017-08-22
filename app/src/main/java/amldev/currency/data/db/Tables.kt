b69c0178fe93ee32bb47781be707d2f20f862ff7
Anartz Mugika <mugan86@gmail.com>
2017-08-22 19:50:55 +0200

    Add MoneyCurrenciesTable primary key column to combine basemoney with conversion money

--- app/src/main/java/amldev/currency/data/db/Tables.kt
+++ app/src/main/java/amldev/currency/data/db/Tables.kt
@@ -8,12 +8,13 @@ package amldev.currency.data.db
 object MoneyInfoTable {
     val NAME = "MoneyInfoTable" //Table Name
     val MONEY = "money"
-    val SYMBOL = "_symbol" // ID
+    val SYMBOL = "symbol" // ID
     val FLAG = "flag"
 }
 
 object MoneyCurrenciesTable {
     val NAME = "MoneyCurrenciesTable" //Table Name
+    val ID = "_id"
     val UPDATED_DATE = "updated_date"
     val ID_BASE = "_id_base"
     val ID_CONVERSION_MONEY = "_id_conversion_money"
