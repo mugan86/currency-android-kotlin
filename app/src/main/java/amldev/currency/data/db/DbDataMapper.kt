ef83a1d1c0e3fdb156f68c4ec573f672cd2d643f
Anartz Mugika <mugan86@gmail.com>
2017-08-22 10:03:09 +0200

    Create DBDataMapper

--- app/src/main/java/amldev/currency/data/db/DbDataMapper.kt
+++ app/src/main/java/amldev/currency/data/db/DbDataMapper.kt
@@ -1,8 +1,14 @@
 package amldev.currency.data.db
 
+import domain.model.Money
+
 /**
  * Created by anartzmugika on 22/8/17.
  */
 class DbDataMapper() {
+
+    fun convertFromDomain(moneys: Money) = with(moneys) {
+        // MoneyInfoTable(symbol, name, icon)
+    }
 
 }
