84c499e318c936c97b15da3ca77007e6aa7cd431
Anartz Mugika <mugan86@gmail.com>
2017-08-28 19:47:12 +0200

    Manage select info from MoneyInfoTable to load in List<Money> inside DbDataMapper

--- app/src/main/java/amldev/currency/data/db/CurrencyDb.kt
+++ app/src/main/java/amldev/currency/data/db/CurrencyDb.kt
@@ -1,6 +1,5 @@
 package amldev.currency.data.db
 
-import amldev.currency.R
 import amldev.currency.extensions.parseList
 import amldev.currency.extensions.toVarargArray
 import domain.model.Money
@@ -10,25 +9,21 @@ import org.jetbrains.anko.db.select
 /***************************************************************************************************
  * Created by anartzmugika on 22/8/17.
  * Define Database operations class.
+ * Options:
+ * Save all money currency items after load from assets JSON File
+ * Extract Currency Money List (if exist items after save when load data from JSON in start)
+ * Get DB Money Currency List size
  ***************************************************************************************************/
 
-class CurrencyDb(
-        val dbHelper: CurrencyDbHelper = CurrencyDbHelper.instance, val dataMapper: DbDataMapper = DbDataMapper()) {
+class CurrencyDb (val dbHelper: CurrencyDbHelper = CurrencyDbHelper.instance,
+                  val dataMapper: DbDataMapper = DbDataMapper()) {
 
     fun saveMoney(money: Money) = dbHelper.use {
-        with(dataMapper.convertMoneyFromDomain(money)) {
-            insert(MoneyInfoTable.NAME, *map.toVarargArray())
-        }
+        with(dataMapper.convertMoneyFromDomain(money)) { insert(MoneyInfoTable.NAME, *map.toVarargArray()) }
     }
 
     fun getMoneyListItems(): List<Money> = dbHelper.use {
-        // select(MoneyInfoTable.NAME, MoneyInfoTable.SYMBOL, MoneyInfoTable.MONEY, MoneyInfoTable.FLAG).parseOpt {  }
-        val items = select(MoneyInfoTable.NAME).parseList { MoneyInfo(HashMap(it)) }
-        var moneys : MutableList<Money> = mutableListOf()
-        items.map {
-            moneys.add(Money(it.symbol, 0.0, it.money, it.flag, R.drawable.ic_united_nations))
-        }
-        return@use moneys
+        return@use dataMapper.getCurrencyMoneysListFromLocalDB(items = select(MoneyInfoTable.NAME).parseList { MoneyInfo(HashMap(it)) })
     }
 
     fun getMoneyListItemsSize () : Int = getMoneyListItems().size
