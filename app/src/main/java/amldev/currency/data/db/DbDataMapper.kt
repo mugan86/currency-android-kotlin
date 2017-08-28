18baf77964fb37a028af5aa28617981a28563a37
Anartz Mugika <mugan86@gmail.com>
2017-08-28 19:46:03 +0200

    Map correctly info to Money Currency List to use in main activity list

--- app/src/main/java/amldev/currency/data/db/DbDataMapper.kt
+++ app/src/main/java/amldev/currency/data/db/DbDataMapper.kt
@@ -1,13 +1,24 @@
 package amldev.currency.data.db
 
+import amldev.currency.R
 import domain.model.Money
 
-/**
+/***************************************************************************************************
  * Created by anartzmugika on 22/8/17.
- */
+ *
+ **************************************************************************************************/
 class DbDataMapper() {
 
     fun convertMoneyFromDomain(money: Money) = with(money) {
         MoneyInfo(symbol, name, flag)
+    }
+
+    fun getCurrencyMoneysListFromLocalDB(items: List<MoneyInfo>) : List<Money> {
+
+        var moneys : MutableList<Money> = mutableListOf()
+        items.map {
+            moneys.add(Money(it.symbol, 0.0, it.money, it.flag, R.drawable.ic_united_nations))
+        }
+        return moneys
     }
 }
