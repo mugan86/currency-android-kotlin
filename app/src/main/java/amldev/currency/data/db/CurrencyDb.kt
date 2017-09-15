77d107f0b87d9493df4d9f329094f032bd8bddae
Anartz Mugika <mugan86@gmail.com>
2017-09-15 20:08:34 +0200

    Add function to save select money currency values

--- app/src/main/java/amldev/currency/data/db/CurrencyDb.kt
+++ app/src/main/java/amldev/currency/data/db/CurrencyDb.kt
@@ -2,6 +2,7 @@ package amldev.currency.data.db
 
 import amldev.currency.extensions.parseList
 import amldev.currency.extensions.toVarargArray
+import domain.model.Currency
 import domain.model.Money
 import org.jetbrains.anko.db.insert
 import org.jetbrains.anko.db.select
@@ -27,5 +28,14 @@ class CurrencyDb (val dbHelper: CurrencyDbHelper = CurrencyDbHelper.instance,
     }
 
     fun getMoneyListItemsSize () : Int = getMoneyListItems().size
+
+    fun saveBaseConversionMoneyValues(currency: Currency) = dbHelper.use {
+        currency.moneyConversion.map { money ->
+            with(dataMapper.convertCurrencyFromDomain(money, currency.baseMoneySymbol)) {
+                insert(MoneyCurrenciesTable.NAME, *map.toVarargArray())
+            }
+        }
+
+    }
 }
 
