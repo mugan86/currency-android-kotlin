757c7b63bcb29d9bb9b606a6c8b7e7b3434e2da7
Anartz Mugika <mugan86@gmail.com>
2017-09-20 21:19:31 +0200

    Remove and check to get data from database

--- app/src/main/java/amldev/currency/domain/model/DomainClasses.kt
+++ app/src/main/java/amldev/currency/domain/model/DomainClasses.kt
@@ -9,7 +9,7 @@ import amldev.currency.R
 data class Currency (val baseMoneySymbol: String, val baseMoneyName: String,
                      val moneyConversion: List<Money>, val date: String) {
 
-    fun getSelectMoneyCurrency(selectMoney: String) : Money = moneyConversion.single(){ (it.symbol == selectMoney) }
+    fun getSelectMoneyCurrency(selectMoney: String) : Money = moneyConversion.single{ (it.symbol == selectMoney) }
 
     fun size() : Int = moneyConversion.size
 
@@ -19,6 +19,5 @@ data class Money (val symbol: String = "",
                   val currencyValue: Double = 0.0,
                   val name: String = "",
                   val flag: String = "united_nations",
-                  val icon: Int = R.drawable.ic_united_nations,
-                  val idConversion: String = "")
+                  val icon: Int = R.drawable.ic_united_nations)
 
