91cfc1092453c3e45730ff0ca9f0868b694077ee
Anartz Mugika <mugan86@gmail.com>
2017-09-20 21:14:15 +0200

    Add new databaseextensions to use with local data base

--- app/src/main/java/amldev/currency/extensions/DatabaseExtensions.kt
+++ app/src/main/java/amldev/currency/extensions/DatabaseExtensions.kt
@@ -5,8 +5,13 @@ package amldev.currency.extensions
  */
 
 import android.database.sqlite.SQLiteDatabase
+import domain.model.Currency
+import domain.model.Money
 import org.jetbrains.anko.db.MapRowParser
 import org.jetbrains.anko.db.SelectQueryBuilder
+import android.database.DatabaseUtils
+
+
 
 fun <T : Any> SelectQueryBuilder.parseList(parser: (Map<String, Any?>) -> T): List<T> =
                parseList(object : MapRowParser<T> {
@@ -19,9 +24,36 @@ fun <T : Any> SelectQueryBuilder.parseOpt(parser: (Map<String, Any?>) -> T): T? 
                     })
 
 fun SQLiteDatabase.clear(tableName: String) {
-        execSQL("delete from $tableName")
+    execSQL("delete from $tableName")
 }
 
 fun SQLiteDatabase.deleteSelect(tableName: String, whereColumn: String, value: String) {
     execSQL("delete from $tableName where $whereColumn = '${value}'")
+}
+
+fun SQLiteDatabase.checkIfValuesUpdate(baseMoney: String): Boolean = DatabaseUtils.longForQuery(this, "SELECT COUNT(_id_base)" +
+        "FROM MoneyCurrenciesTable WHERE _id_base = ? AND updated_date = ?",  arrayOf(baseMoney, DateTime.currentData)).toInt() > 0
+
+fun SQLiteDatabase.getMoneyWithCurrencies(baseMoney: String): Currency {
+    val cursor = rawQuery("SELECT MCT._id_conversion_money, value_conversion, flag, updated_date " +
+            "FROM MoneyCurrenciesTable MCT, MoneyInfoTable MT WHERE MCT._id_base = MT.symbol AND MCT._id_base = ?", arrayOf(baseMoney))
+
+    val moneyList = mutableListOf<Money>()
+    var currencyName = ""
+    var update_date  = ""
+    try {
+        if (cursor.moveToFirst()) {
+            do {
+                if (currencyName == "") {
+                    currencyName = cursor.getString(2)
+                    update_date = cursor.getString(3)
+                }
+                moneyList.add(Money(cursor.getString(0), cursor.getDouble(1), "", cursor.getString(2)))
+            } while (cursor.moveToNext())
+        }
+    } finally {
+        cursor.close()
+    }
+
+    return Currency(baseMoney, currencyName, moneyList, update_date)
 }
