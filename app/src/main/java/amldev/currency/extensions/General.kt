70b3da7624e1a6c48e7fdfa54c46d8c732433493
Anartz Mugika <mugan86@gmail.com>
2018-01-13 20:12:33 +0100

    Optimize navigate function and add extras :)

--- app/src/main/java/amldev/currency/extensions/General.kt
+++ app/src/main/java/amldev/currency/extensions/General.kt
@@ -1,9 +1,11 @@
 package amldev.currency.extensions
 
 import amldev.currency.App
+import amldev.currency.domain.model.Extra
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 /**
  * Created by anartzmugika on 2/1/18.
@@ -17,8 +19,12 @@ import android.content.Intent
  *
  * Gracias a las funciones reified, esto va a cambiar.
  *****************************************************************************************************************/
-inline fun <reified T : Activity> Context.startActivity() {
-    startActivity(Intent(this, T::class.java))
+inline fun <reified T : Activity> Activity.navigate(extras: List<Extra> = emptyList<Extra>(), finishThisActivity: Boolean = true) {
+    val i = Intent(this, T::class.java)
+    extras.map { data -> i.putExtra(data.label, data.value) }
+    startActivity(i)
+    if (finishThisActivity) finish()
+    overridePendingTransition(0, 0)
 }
 
 
@@ -27,3 +33,11 @@ val Activity.app: App
 
 val Context.context: Context
     get() = applicationContext as Context
+
+/**
+ * Created by anartzmugika on 23/11/17.
+ * More info: https://antonioleiva.com/kotlin-android-extension-functions/vhttps://antonioleiva.com/kotlin-android-extension-functions/
+ */
+fun Context.showMessage(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
