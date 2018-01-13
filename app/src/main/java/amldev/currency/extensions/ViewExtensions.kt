9ad6cf3c1b1a44cf8784f6a5035ad59f179eb3fe
Anartz Mugika <mugan86@gmail.com>
2018-01-13 09:25:36 +0100

    Add new View extensions

--- app/src/main/java/amldev/currency/extensions/ViewExtensions.kt
+++ app/src/main/java/amldev/currency/extensions/ViewExtensions.kt
@@ -4,7 +4,18 @@ package amldev.currency.extensions
  * Created by Anartz Mugika (mugan86@gmail.com) on 31/7/17.
  ************************/
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 
 val View.ctx: Context
     get() = context
+
+fun ViewGroup.inflate(resource: Int): View =
+        LayoutInflater.from(context).inflate(resource, this, false)
+
+inline fun <reified T : View> View.find(id: Int): T = findViewById<T>(id)
+
+fun View.show() = run { visibility = View.VISIBLE }
+
+fun View.hide() = run { visibility = View.GONE }
