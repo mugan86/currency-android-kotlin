b478422cf7a810235d0cf1e3e9a71c4ae9a50114
Anartz Mugika <mugan86@gmail.com>
2018-01-13 09:26:45 +0100

    Remove ViewHolder after this class move to "holders"

--- app/src/main/java/amldev/currency/ui/adapters/MoneyAdapter.kt
+++ app/src/main/java/amldev/currency/ui/adapters/MoneyAdapter.kt
@@ -1,15 +1,13 @@
 package amldev.currency.ui.adapters
 
 import amldev.currency.R
+import amldev.currency.domain.model.Money
+import amldev.currency.extensions.MoneyItem
 import amldev.currency.extensions.ctx
-import android.content.Context
+import amldev.currency.ui.adapters.holders.ViewHolder
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.squareup.picasso.Picasso
-import amldev.currency.domain.model.Money
-import kotlinx.android.synthetic.main.item_money.view.*
 
 /***************************************************************
  * Created by anartzmugika on 31/7/17.
@@ -17,8 +15,8 @@ import kotlinx.android.synthetic.main.item_money.view.*
  * Adapter to load money info in RecyclerView List with custom flag
  *******************************************************************/
 class MoneyAdapter (
-        val moneyList: List<Money>, val itemClick: (Money) -> Unit)
-    : RecyclerView.Adapter<MoneyAdapter.ViewHolder>() {
+        val moneyList: List<Money>, val itemClick: MoneyItem)
+    : RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_money, parent, false)
         return ViewHolder(view, itemClick)
@@ -30,21 +28,6 @@ class MoneyAdapter (
     }
 
     override fun getItemCount(): Int = moneyList.size
-
-    class ViewHolder(view: View,
-                     val itemClick: (Money) -> Unit) : RecyclerView.ViewHolder(view) {
-
-        fun bindForecast(money: Money) {
-            with(money) {
-                Picasso.with(itemView.ctx).load(getFlagDrawable(itemView.ctx, flag)).into(itemView.icon)
-                itemView.symbolTextView.text = symbol
-                itemView.nameTextView.text = name
-                itemView.setOnClickListener { itemClick(this) }
-            }
-        }
-        private fun getFlagDrawable(context: Context, flag: String) =
-                context.resources.getIdentifier("ic_$flag", "drawable", context.packageName)
-    }
 
     interface OnItemClickListener {
         operator fun invoke(money: Money)
