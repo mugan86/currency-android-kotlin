6a0ead4fd750cdfecb51a0afa9b2b76a37d38c23
Anartz Mugika <mugan86@gmail.com>
2018-01-14 07:55:05 +0100

    Add new TypeAliasExtensions

--- app/src/main/java/amldev/currency/extensions/TypeAliasExtensions.kt
+++ app/src/main/java/amldev/currency/extensions/TypeAliasExtensions.kt
@@ -3,12 +3,13 @@ package amldev.currency.extensions
 /**
  * Created by anartzmugika on 12/1/18.
  */
+import amldev.currency.domain.model.Currency
 import amldev.currency.domain.model.Money
 
-typealias MoneyItem = (Money) -> Unit
+typealias MoneyItemUnit = (Money) -> Unit
 
 typealias MoneysListUnit = (List<Money>) -> Unit
 
 typealias MoneysList = List<Money>
 
-typealias MoneyItems = List<MoneyItem>
+typealias CurrencyUnit = (Currency) -> Unit
