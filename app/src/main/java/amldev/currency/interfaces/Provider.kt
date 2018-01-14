cbbcc67a98da5065eccb847a83f8d0add2755c17
Anartz Mugika <mugan86@gmail.com>
2018-01-14 08:25:15 +0100

    Add in param pass context

--- app/src/main/java/amldev/currency/interfaces/Provider.kt
+++ app/src/main/java/amldev/currency/interfaces/Provider.kt
@@ -3,11 +3,12 @@ package amldev.currency.interfaces
 import amldev.currency.domain.model.Currency
 import amldev.currency.extensions.CurrencyUnit
 import amldev.currency.extensions.MoneysListUnit
+import android.content.Context
 
 /**
  * Created by anartzmugika on 13/1/18.
  */
 interface Provider {
     fun loadCurrenciesList(f: MoneysListUnit)
-    fun loadMoneySelectData(selectCurrency: Currency, f: CurrencyUnit)
+    fun loadMoneySelectData(selectCurrency_: Currency, context: Context, f: CurrencyUnit)
 }
