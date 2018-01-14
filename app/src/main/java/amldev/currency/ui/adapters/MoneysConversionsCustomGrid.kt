32b184d5bc40ee707ce176d2fda75f926bd1857b
Anartz Mugika <mugan86@gmail.com>
2018-01-14 08:26:45 +0100

    Add observable value in MoneyConversions Grid

--- app/src/main/java/amldev/currency/ui/adapters/MoneysConversionsCustomGrid.kt
+++ app/src/main/java/amldev/currency/ui/adapters/MoneysConversionsCustomGrid.kt
@@ -1,22 +1,27 @@
 package amldev.currency.ui.adapters
 
 import amldev.currency.R
+import amldev.currency.domain.model.Currency
+import amldev.currency.domain.model.Money
+import amldev.currency.extensions.ctx
+import amldev.currency.extensions.numberInStringFormatWithDecimals
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.grid_item_money_conversion.view.*
-import amldev.currency.extensions.ctx
-import amldev.currency.extensions.numberInStringFormatWithDecimals
-import amldev.currency.domain.model.Currency
-import amldev.currency.domain.model.Money
+import kotlin.properties.Delegates
 
 /**
  * Created by anartzmugika on 3/8/17.
  */
 
-class MoneysConversionsCustomGrid(private val moneys: List<Money>, private val currency: Currency, private val inputValue: Double) : BaseAdapter() {
+class MoneysConversionsCustomGrid(moneys: List<Money>, private val currency: Currency, private val inputValue: Double) : BaseAdapter() {
+
+    var moneys: List<Money> by Delegates.observable(moneys, { _, _, _ ->
+        notifyDataSetChanged()
+    })
 
     override fun getCount(): Int = moneys.size
 
