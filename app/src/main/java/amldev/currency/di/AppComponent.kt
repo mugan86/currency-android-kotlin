77796e579972e9f23f895f9571cdd0dfa9cabf42
Anartz Mugika <mugan86@gmail.com>
2018-01-14 07:31:10 +0100

    Add new Component

--- app/src/main/java/amldev/currency/di/AppComponent.kt
+++ app/src/main/java/amldev/currency/di/AppComponent.kt
@@ -2,6 +2,8 @@ package amldev.currency.di
 
 import amldev.currency.App
 import amldev.currency.ui.activities.main.di.MainModule
+import amldev.currency.ui.activities.money_conversions.di.SelectMoneyConversionsComponent
+import amldev.currency.ui.activities.money_conversions.di.SelectMoneyConversionsModule
 import amldev.kotlinandroidonlinecourse.ui.activities.main.di.MainComponent
 import dagger.Component
 import javax.inject.Singleton
@@ -14,4 +16,5 @@ import javax.inject.Singleton
 interface AppComponent {
     fun inject(app: App)
     fun plus(mainModule: MainModule): MainComponent
+    fun plus(selectMoneyModule: SelectMoneyConversionsModule): SelectMoneyConversionsComponent
 }
