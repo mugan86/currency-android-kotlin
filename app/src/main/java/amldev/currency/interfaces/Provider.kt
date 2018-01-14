f297a7fcc1ff4f6fd0ba98b6827941d5746caf16
Anartz Mugika <mugan86@gmail.com>
2018-01-14 07:26:52 +0100

    Add new provider function and update with correct name first function

--- app/src/main/java/amldev/currency/interfaces/Provider.kt
+++ app/src/main/java/amldev/currency/interfaces/Provider.kt
@@ -1,10 +1,13 @@
 package amldev.currency.interfaces
 
+import amldev.currency.domain.model.Currency
+import amldev.currency.extensions.CurrencyUnit
 import amldev.currency.extensions.MoneysListUnit
 
 /**
  * Created by anartzmugika on 13/1/18.
  */
 interface Provider {
-    fun dataAsync(f: MoneysListUnit)
+    fun loadCurrenciesList(f: MoneysListUnit)
+    fun loadMoneySelectData(selectCurrency: Currency, f: CurrencyUnit)
 }
