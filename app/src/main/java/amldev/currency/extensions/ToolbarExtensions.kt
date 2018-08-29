b08b64ed480bb1e3c006d032fd3057a7d6281fc6
Anartz Mugika <mugan86@gmail.com>
2018-08-29 19:57:12 +0200

    Replace deprecated code with new implementations

--- app/src/main/java/amldev/currency/extensions/ToolbarExtensions.kt
+++ app/src/main/java/amldev/currency/extensions/ToolbarExtensions.kt
@@ -1,6 +1,7 @@
 package amldev.currency.extensions
 
 import amldev.currency.R
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.toolbar.*
@@ -8,8 +9,9 @@ import kotlinx.android.synthetic.main.toolbar.*
 /********************************************************************************
  * Created by Anartz Mugika (mugan86@gmail.com) on 13/1/18.
  ********************************************************************************/
-fun AppCompatActivity.addToolbar(color: Int = resources.getColor(R.color.White), title: String = resources.getString(R.string.app_name), subtitle: String = "",
-                                 navigationIcon: Int = R.drawable.ic_back, backButton: Boolean = false) {
+fun AppCompatActivity.addToolbar(color: Int = ActivityCompat.getColor(context, R.color.White),
+                                 title: String = resources.getString(R.string.app_name), subtitle: String = "",
+                                 navigationIcon: Int = R.drawable.ic_arrow_back, backButton: Boolean = false) {
     supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
     toolbar.title = title
@@ -18,7 +20,7 @@ fun AppCompatActivity.addToolbar(color: Int = resources.getColor(R.color.White),
     setSupportActionBar(toolbar)
 
     if (backButton) {
-        toolbar.navigationIcon = resources.getDrawable(navigationIcon)
+        toolbar.navigationIcon = ActivityCompat.getDrawable(context, navigationIcon)
         toolbar.setNavigationOnClickListener(View.OnClickListener {
             //What to do on back clicked
             finish()
