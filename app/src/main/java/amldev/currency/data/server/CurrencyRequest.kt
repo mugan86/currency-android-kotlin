94b70c92a53bfde6bbb947a1644b3fe230cf958e
Anartz Mugika <mugan86@gmail.com>
2018-08-30 08:12:16 +0200

    Add new currencies in comment to future and load from JSOn file if internet source not working

--- app/src/main/java/amldev/currency/data/server/CurrencyRequest.kt
+++ app/src/main/java/amldev/currency/data/server/CurrencyRequest.kt
@@ -20,13 +20,22 @@ import java.net.URL
 class CurrencyRequest(private val baseMoney: String = "EUR") {
     companion object {
         const val symbols = "AUD,CAD,CHF,CNY,EUR,GBP,INR,JPY,MYR,RUB,SGD,USD"
+        // BRL,DKK,ILS,HKD
+        // https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html
         private const val URL_LOCALHOST = "https://api.exchangeratesapi.io/latest?symbols=$symbols&base="
         // https://api.exchangeratesapi.io/latest?symbols=AUD,CAD,CHF,CNY,EUR,GBP,INR,JPY,MYR,RUB,SGD,USD&base=GBP
     }
 
     fun execute(context:Context) : CurrencyResult {
         if (isNetworkConnected(context) && DataPreference.getPreferenceBoolean(context, Constants.USE_INTERNET)) {
-            return Gson().fromJson(URL(URL_LOCALHOST + baseMoney).readText(), CurrencyResult::class.java)
+            println("Take data from internet!!")
+            var currencyResult: CurrencyResult
+            try {
+                currencyResult = Gson().fromJson(URL(URL_LOCALHOST + baseMoney).readText(), CurrencyResult::class.java)
+            } catch (e: Exception) {
+                currencyResult = Gson().fromJson(getJSONResource(context, "currencies/${baseMoney.toLowerCase()}"), CurrencyResult::class.java)
+            }
+            return currencyResult
         }
         //Without Internet, take assets/currencies/baseMoney.json file
         return Gson().fromJson(getJSONResource(context, "currencies/${baseMoney.toLowerCase()}"), CurrencyResult::class.java)
